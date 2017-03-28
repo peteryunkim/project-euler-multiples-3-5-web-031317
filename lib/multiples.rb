270e6f9dbd245cdb2ee6268b91f6374467ce9256
github username <github email address>
2017-03-28 11:12:48 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
-# Enter your procedural solution here!
+# # Enter your procedural solution here!
+# mr_euler = (1..999).select do |num|
+#   num % 3 == 0 || num % 5 == 0
+#   end
+#
+# mr_euler.reduce {|sum, n| sum+= n}
+
+def collect_multiples(limit)
+  (1...limit).select do |num|
+    num % 3 == 0 || num % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce {|sum, n| sum += n}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,34 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...self.limit).select do |num|
+      num % 3 == 0 || num % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    collect_multiples.reduce {|sum, n| sum += n}
+  end
+
+end
+
+# 10 = Multiples.new(10)
+#<Multiple#3209rdv90ujwcsdlkj limit = 10 >
+# 10.collect_multiples
+#
+# def collect_multiples(limit)
+#   (1...limit).select do |num|
+#     num % 3 == 0 || num % 5 == 0
+#   end
+# end
+#
+# def sum_multiples(limit)
+#   collect_multiples(limit).reduce {|sum, n| sum += n}
+# end
